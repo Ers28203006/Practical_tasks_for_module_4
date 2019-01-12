e8344a637f4b578f05814481d360a5bcd7ae61ba
ersyn_sultanbaev <ersyn_sultanbaev@mail.ru>
2019-01-12 09:14:51 +0600

    Добавьте файлы проекта.

---
+++ Practical_Work_4/Money.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Work_4
+{
+    class Money
+    {
+        /* Поля:
+                * Номинал купюры
+                * Количество купюр */
+
+        private double _nominalBanknotes;
+        private int _amountBanknotes;
+        private double _costOfGoods;
+
+
+        /* Конструктор */
+
+        public Money(double nominalBanknotes, int amountBanknotes)
+        {
+            _nominalBanknotes = nominalBanknotes;
+            _amountBanknotes = amountBanknotes;
+            _costOfGoods = _amountBanknotes * _nominalBanknotes;
+        }
+
+        /* Свойства */
+
+        public double GetNominalBanknotes()
+        {
+            return _nominalBanknotes;
+        }
+
+        public void SetNominalBanknotes(double nominalBanknotes)
+        {
+            _nominalBanknotes = nominalBanknotes;
+        }
+
+        public int GetAmountBanknotes()
+        {
+            return _amountBanknotes;
+        }
+
+        public void SetAmountBanknotes(int amountBanknotes)
+        {
+            _amountBanknotes = amountBanknotes;
+        }
+
+        /* Методы */
+
+        public void ShowBankotes()
+        {
+            Console.Write("Номинал купюр: ");
+            Console.WriteLine(_nominalBanknotes);
+            Console.Write("Количестов купюр: ");
+            Console.WriteLine(_amountBanknotes);
+        }
+
+        public void IsThereNoughMoney(double costOfGoods)
+        {
+            
+            if (costOfGoods > _costOfGoods)
+            {
+                Console.WriteLine("Денежных средств на покупку товара не достаточно.");
+            }
+
+            else
+            {
+                Console.WriteLine("Денежных средств на покупку товара достаточно");
+            }
+        }
+
+        public void HowMuchToBuy(double costOfGoods)
+        {
+            int quantityOfGoods = (int)(_costOfGoods / costOfGoods);
+            Console.Write("На сумму имеющуюся в кошельке можно купить ");
+            Console.Write(quantityOfGoods);
+            Console.WriteLine(" штук товара");
+        }
+
+
+    }
+}
---
+++ Practical_Work_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Work_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nominalBanknotes;
+            int amountBanknotes;
+
+            Console.Write("Введите номинал купюр: ");
+            nominalBanknotes = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Введите количество купюр номинала: ");
+            amountBanknotes = int.Parse(Console.ReadLine());
+
+            Money money=new Money(nominalBanknotes, amountBanknotes);
+
+            double costOfGoods;
+
+            Console.WriteLine();
+            Console.Write("Введите сколько стоит товар: ");
+            costOfGoods = double.Parse(Console.ReadLine());
+
+            money.IsThereNoughMoney(costOfGoods);
+
+            money.HowMuchToBuy(costOfGoods);
+
+            Console.ReadLine();
+        }
+    }
+}
